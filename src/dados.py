d436e9f2c1f1b86d861347571e6d5cf98102f4c8
Samantha Spezamilgio <sanvic77@gmail.com>
2025-08-06 10:23:25 -0300

    dados

---
+++ src/dados.py
@@ -0,0 +1,245 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+from sklearn.model_selection import train_test_split
+from sklearn import preprocessing
+from sklearn.linear_model import LinearRegression
+
+np.set_printoptions(threshold=None, precision=2)
+pd.set_option('display.max_columns', 500)
+pd.set_option('display.max_rows', 500)
+pd.set_option('precision', 2)
+
+from google.colab import files
+
+uploaded = files.upload()
+
+
+titanic_train = pd.read_csv(next(iter(uploaded.keys())))
+
+print("\nDimensões de Titanic:\n{0}\n".format(titanic_train.shape))
+print("\nCampos de Titanic:\n{0}\n".format(list(titanic_train.keys())))
+print("\nTipos dos dados:\n{0}\n".format(titanic_train.dtypes))
+
+# Exibe apenas os campos numméricos:
+
+print(titanic_train.describe())
+
+# Para se ter uma visão dos atributos categóricos, os atributos não numéricos 
+# são descartados. 
+
+categ = titanic_train.dtypes[titanic_train.dtypes == "object"].index
+
+print("\n", titanic_train[categ].describe(), sep='\n')
+
+del titanic_train["PassengerId"]
+del titanic_train["Ticket"]
+
+# Verifique que o número de atributos reduziu para 10.
+
+print("\nDimensões de Titanic:\n{0}\n".format(titanic_train.shape))
+
+# OBSERVAÇÃO: se a base for usada para a competição do kaggle, o atributo
+# alvo, que é o campo 'survived' não deve ser recodificada.
+
+new_survived = pd.Categorical(titanic_train["Survived"])
+new_survived = new_survived.rename_categories(["Morreu","Sobreviveu"])              
+titanic_train["Survided"] = new_survived
+
+new_Pclass = pd.Categorical(titanic_train["Pclass"], ordered=True)
+new_Pclass = new_Pclass.rename_categories(["1aClasse","2aClasse","3aClasse"])     
+titanic_train["Pclass"] = new_Pclass
+
+print("\nTipos dos dados:\n{0}\n".format(titanic_train.dtypes))
+categ = titanic_train.dtypes[titanic_train.dtypes == "category"].index
+print("\n", titanic_train[categ].describe(), sep='\n')
+
+#Exibe valores únicos
+
+print("\nValores únicos do atributo Cabin:",titanic_train["Cabin"].unique(), sep='\n')
+
+# Converte o dado para String
+
+char_cabin = titanic_train["Cabin"].astype(str)
+
+# Pega apenas a primeira letra
+
+new_cabin = pd.Categorical([cabin[0] for cabin in char_cabin])
+titanic_train["Cabin"] = new_cabin
+
+print("\nValores únicos do atributo Cabin:",titanic_train["Cabin"].unique(), sep='\n')
+
+titanic_train.hist(column='Age',    # Coluna a ser plotada
+                   figsize=(9,6),   # Tamanho do gráfico
+                   bins=20)         # Numero de colunas do histogram
+
+mediana = np.median([el for el in titanic_train["Age"] if (np.isnan(el) == False)])
+
+new_age = np.where(titanic_train["Age"].isnull(), # condição
+                   mediana,                       # valor se verdadeiro
+                   titanic_train["Age"])          # valor se falso
+titanic_train["Age"] = new_age
+
+print("\nAnálise do novo atributo Age:")
+print(titanic_train["Age"].describe())
+
+titanic_train.hist(column='Age',    # Coluna a ser plotada
+                   figsize=(9,6),   # Tamanho do gráfico
+                   bins=20)         # Numero de colunas do histogram
+
+titanic_train["Fare"].plot(kind="box", figsize=(9,9))
+
+index = np.where(titanic_train["Fare"] == max(titanic_train["Fare"]) )
+
+print("Registros com valores extremos:",titanic_train.loc[index], sep='\n')
+
+titanic_train["Family"] = titanic_train["SibSp"] + titanic_train["Parch"]
+
+# Encontrando quem tem a maior família À bordo
+
+most_family = np.where(titanic_train["Family"] == max(titanic_train["Family"]))
+
+print("\nAs maiores famílias à bordo:\n{0}".format(titanic_train.loc[most_family]))
+
+int_fields = titanic_train.dtypes[titanic_train.dtypes == "int64"].index
+corr = np.corrcoef(titanic_train[int_fields].transpose())
+correlacao = pd.DataFrame(data=corr, index=int_fields, columns=int_fields)
+
+print("\nMatriz de correlação dos atributos inteiros:\n{0}".format(correlacao))
+
+from google.colab import files
+
+uploaded = files.upload()
+
+gazola = pd.read_excel(next(iter(uploaded.keys())), sheet_name=1)
+
+print("\nDimensões:\n{0}\n".format(gazola.shape))
+print("\nCampos:\n{0}\n".format(list(gazola.keys())))
+print("\nTipos dos dados:\n{0}\n".format(gazola.dtypes))
+print(gazola.describe(percentiles=[]))
+
+X = gazola.iloc[:,1:(gazola.shape[1] - 2)]
+
+y = gazola.iloc[:,(gazola.shape[1] - 1)]
+
+# Recupera os nomes dos atributos
+
+atributos = list(gazola)[1:(gazola.shape[1] - 2)]
+rotulo = list(gazola)[(gazola.shape[1] - 1)]
+
+# Exibe o histograma dos atributos.
+
+fig, ax = plt.subplots(2, 3, figsize=(10, 6))
+plt.suptitle("Histograma dos atributos")
+
+for i in range(2):
+    for j in range(3):
+        ax[i, j].hist(X.iloc[:,(i*3 + j)], label=atributos[i*3+j], bins=30)
+        ax[i, j].legend()
+
+fig, ax = plt.subplots(2, 3, figsize=(10, 6))
+plt.suptitle("Histograma dos atributos")
+
+for i in range(2):
+    for j in range(3):
+        if j == 2 and i == 1:
+            ax[i, j].hist(y.iloc[:],label=rotulo,bins=30)
+        else:            
+            ax[i, j].hist(X.iloc[:,(i*3 + j+6)],label=atributos[i*3 + j+6], bins=30)
+        ax[i, j].legend()
+
+
+# Amostragem de dados
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1, random_state=0)
+
+print("Base de treinamento:{0}".format(X_train.shape))
+print("Base de teste:{0}".format(X_test.shape))
+
+lnr = LinearRegression().fit(X_train, y_train)
+
+y_prev = lnr.predict(X_test)
+
+print("Acurácia da base de treinamento: {:.2f}".format(lnr.score(X_train, y_train)))
+print("Acurácia da base de testes: {:.2f}".format(lnr.score(X_test, y_test)))
+print("Descrição do modelo: ")
+s = ["{0}: {1:0.2f}".format(a, v) for a, v in zip(atributos, lnr.coef_)]
+print("w: {}  b: {:.2f}".format(s, lnr.intercept_))
+print("Número de atributos usados: {}".format(np.sum(lnr.coef_ != 0)))
+
+# Calcula o erro absoluto e o erro percentual da regressao linear
+errolnr = np.abs(y_test - y_prev)
+erroperc = errolnr / list(y_test)
+
+print('Erro percentual:\n Média: {0:.2f}  Max: {1:.2f}   Min: {2:.2f}'
+      .format(np.mean(erroperc), np.max(erroperc), np.min(erroperc)))
+
+plt.figure(figsize=(6, 4))
+plt.errorbar(np.arange(y_test.size), list(y_test), yerr=errolnr,
+             fmt='.', ecolor='r', capsize=3)
+plt.title("Valores reais (barras de erro de predição)")
+plt.grid()
+
+fig, ax = plt.subplots(1, 2, figsize=(7, 4))
+plt.suptitle("Erros de previsão")
+ax[0].plot(errolnr,'.')
+ax[0].set_xlabel("Erro absoluto")
+ax[0].grid()
+ax[1].plot(erroperc,'.')
+ax[1].set_xlabel("Erro percentual")
+ax[1].grid()
+
+
+# Os atributos possuem faixas de valores diferentes, que influencia os pesos 
+# dos coeficientes da regressão. Vamos trabalhar melhor os atributos.
+
+X_scale = X
+
+# Iremos aplicar o logaritmo em "Energia total", "Área total" e "Preço $".
+
+X_scale['Energia'] = np.log10(X['Energia'])
+X_scale['Artot'] = np.log10(X['Artot'])
+y_scale = np.log10(y)
+
+# Normalização Min-Max dos dados.
+
+min_max_scaler = preprocessing.MinMaxScaler()
+X_scale = min_max_scaler.fit_transform(X_scale)
+
+fig, ax = plt.subplots(1, 2, figsize=(6, 4))
+ax[0].hist(y,bins=30, label='Preço $')
+ax[0].set_title('Preço $')
+ax[1].hist(y_scale,bins=30, label='log10(Preço $)')
+ax[1].set_title('log_10(Preço $)')
+
+
+X_train2, X_test2, y_train2, y_test2 = train_test_split(X_scale, y_scale, test_size=0.1, random_state=0)
+
+lnr2 = LinearRegression().fit(X_train2, y_train2)
+
+y_prev2 = lnr2.predict(X_test2)
+
+errolnr2 = np.abs(y_test2 - y_prev2)
+errolnr2perc = np.abs(y_test2 - y_prev2)/y_test2
+
+plt.figure(figsize=(6, 4))
+plt.plot(erroperc,'o', label='Regressão com atrib. originais')
+plt.plot(errolnr2perc,'o', label='Regressão com atrib. normalizados')
+plt.title("Erro de previsão (em %)")
+plt.legend()
+
+print("\n--------------- Regressão Linear Normalizada ---------------")
+print("Acurácia da base de treinamento: {:.2f}".format(lnr2.score(X_train2, y_train2)))
+print("Acurácia da base de testes: {:.2f}".format(lnr2.score(X_test2, y_test2)))
+print("Descrição do modelo: ")
+s = ["{0}: {1:0.2f}".format(a, v) for a, v in zip(atributos, lnr2.coef_)]
+print("w: {}  b: {:.2f}".format(s, lnr2.intercept_))
+
+
+print("\n------------------  Comparação de pesos   ------------------")
+s = ["{0}: {1:0.2f}".format(a, v) for a, v in zip(atributos, lnr.coef_)]
+print("Original:\n w: {}  b: {:.2f}".format(s, lnr.intercept_))
+s = ["{0}: {1:0.2f}".format(a, v) for a, v in zip(atributos, lnr2.coef_)]
+print("Normalizado:\n w: {}  b: {:.2f}".format(s, lnr2.intercept_))
